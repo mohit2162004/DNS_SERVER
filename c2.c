85e1efb055d461180ad0e5b20e2f55230e3579d0
mohit <mohitbarole112345@gmail.com>
2024-09-19 10:57:13 +0530

    second commit

--- c2.c
+++ c2.c
@@ -6,12 +6,14 @@ int main() {
     long long c;  // equivalent to long long int c;
     double e;
     long double f;
+    char ch;
 
     printf("Size of int = %zu bytes \n", sizeof(a));
     printf("Size of long int = %zu bytes\n", sizeof(b));
     printf("Size of long long int = %zu bytes\n", sizeof(c));
     printf("Size of double = %zu bytes\n", sizeof(e));
     printf("Size of long double = %zu bytes\n", sizeof(f));
+    printf("Size of long double = %zu bytes\n", sizeof(ch));
     
     return 0;
 }
